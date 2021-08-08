cadbe63bf8450c220b830ac454f2ca7a266a78f0
root <abhilashmadhu24@gmail.com>
2021-08-08 04:18:19 +0000

    first commit

--- host.sh
+++ host.sh
@@ -1,39 +1,56 @@
 #!/bin/bash
-#######################################
+############################################################################################################
 #SCRIPT NAME            :host.sh
 #DESCRIPTION            :Host domain on this server by adding on host file and creating document root
-
 #USAGE                  :run sh host.sh
-#                       :give domain name and document root on prompt
-#######################################
+#                       :give domain name, document root and FTP username on prompt
+############################################################################################################
 
-cd /usr/local/scripts/hosting_script
+IPADDR=$(hostname -I | awk '{print $1}')
+green='\033[0;32m'
+clean='\033[0m'
+yellow='\033[0;33m'
+red='\033[0;31m'
 
-rm -rf default_httpdssl.txt default_htaccess.txt default_httpd.txt > /dev/null 2>&1
+
+begin_ssl()
+
+{
+      grep "$DOMAIN" /$LOC/etc/httpd.conf | grep www
+      if [ $? != 0 ];
+      then
+      echo "SSL without www"
+      certbot certonly --debug --webroot -w /opt/lampp/htdocs/$DOMAIN -d $DOMAIN --register-unsafely-without-email --agree-tos
+      else
+      echo "SSL with www"
+      certbot certonly --debug --webroot -w /opt/lampp/htdocs/$DOMAIN -d $DOMAIN --register-unsafely-without-email --agree-tos
+      fi
+}
+
+rm -rf default_httpdssl.txt default_htaccess.txt default_httpd.txt .htaccess > /dev/null 2>&1
 
 cp default_httpd default_httpd.txt
 cp default_httpdssl default_httpdssl.txt
 cp default_htaccess default_htaccess.txt
-
-
-echo "###########################################################"
-printf "\n \n"
-printf " Confirm wheather you like to add a new Domain or a sub-domain\n"
+cp default_index default_index.txt
+echo "
+${yellow}###########################################################${clean}"
+echo " Confirm wheather you like to add a new Domain or a sub-domain  "
 echo "1 : Domain"
 echo "2 : Sub-domain"
 
 read -p "Enter your choice.. 1/2 : " CHOICE
-
 if [ "$CHOICE" = "1" ]; then
           read -p "Domain name : " DOMAIN
-
+          sed -i "s/IPADDR/$IPADDR/g" default_httpd.txt
 	  sed -i "s/DOMAIN/$DOMAIN/g" default_httpd.txt
           sed -i "s/DOMAIN/$DOMAIN/g" default_htaccess.txt
           sed -i "s/DOMAIN/$DOMAIN/g" default_httpdssl.txt
+	  sed -i "s/IPADDR/$IPADDR/g" default_httpdssl.txt
 
 elif [ "$CHOICE" = "2" ]; then
               read -p "Sub-domain name : " DOMAIN
-
+              sed -i "s/IPADDR/$IPADDR/g" default_httpd.txt
               sed -i "s/www.//g" default_httpd.txt
               sed -i "s/DOMAIN/$DOMAIN/g" default_httpd.txt
 
@@ -42,153 +59,180 @@ elif [ "$CHOICE" = "2" ]; then
 
 	      sed -i "s/www.//g" default_httpdssl.txt
 	      sed -i "s/DOMAIN/$DOMAIN/g" default_httpdssl.txt
+	      sed -i "s/IPADDR/$IPADDR/g" default_httpdssl.txt
               
 else
-  echo "Aborted!!!";
+	     echo "Aborted!!!";
 fi
 
-LOC="/opt/lampp"
-HLOC="$LOC/htdocs/$DOMAIN"
+	     LOC="/opt/lampp"
+	     HLOC="$LOC/htdocs/$DOMAIN"
+             read -p "Give your FTP USER_NAME : " USER
+
+		if [ -z "$DOMAIN" ];
+       		then
+       		echo "${red}Domain name could not be empty${clean}"
+       		echo "Try again!!!!!"
+       		elif grep -q "ServerAlias $DOMAIN" $LOC/etc/httpd.conf ; then
+			echo ""
+  			echo "${yellow}#####################################################################################################################${clean}"
+       			echo "Domain already found on configuration Please manually check /opt/lampp/etc/httpd.conf !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"
+       			echo "${yellow}#####################################################################################################################${clean}"
+ 
+       		else    echo ""
+			echo "${green}###############################################${clean}"
+       			echo "Domain name = $DOMAIN"
+       			echo "Document root location = $HLOC "
+       			echo "${green}###############################################${clean}"
+			sleep 1
+      
+			DATE=$(date +"%Y-%m-%d-%H-%M-%S")
+
+      			mkdir -p $LOC/etc/httpd_backups/
+      			cp $LOC/etc/httpd.conf $LOC/etc/httpd_backups/httpd.conf-"$DATE"
+
+      			echo "Creating directory and assigning ownership "
+     			 id -u $USER 
+         		if [ $? -eq 0 ];then
+	   			echo "USER $USER ALREADY EXISTS.....PLEASE USE OLD PASSWORD"
+	   			echo "$USER" "$DOMAIN" > ftp_details
+         		else  	
+           		PASS=$(head -n 4096 /dev/urandom | tr -dc a-zA-Z0-9 | cut -b 1-16)
+           		useradd -m $USER
+	     		if [ $? -eq 0 ];then
+	     		echo "$USER:$PASS" | chpasswd
+	     		echo $DOMAIN $USER : $PASS > ftp_details
+	     		fi
+         		fi
+
+      
+     			mkdir -p "$HLOC"
+			mv default_index.txt $HLOC/index.html
+     			chown -R $USER.$USER "$HLOC"
+			chmod -R 644  "$HLOC"
+     			echo "Domain added to conf file. Previous configuration file is copied on $LOC/etc/httpd_backups/httpd.conf-$DATE"
+     			sleep 1
+
+     			cat default_httpd.txt  >> $LOC/etc/httpd.conf
+     			echo "Checking configuration file...."
+
+     			/opt/lampp/bin/apachectl -t
+     			if [ $? != 0 ]
+     				then
+     					echo "
+					${red}Errors found on configuration file. Process reversed${clean}"
+     					mv $LOC/etc/httpd_backups/httpd.conf-"$DATE" /$LOC/etc/httpd.conf
+     					echo "Checking for any current syntax errors....."
+     					/opt/lampp/bin/apachectl -t
+     					if [ $? != 0 ]
+           					then echo "
+							${red}URGENT ERROR PLEASE CHECK CONFIGURATION BEFORE RELOADIN SERVICE${clean}"
+     					fi
+     			else 
+	 			echo "Reloading service..."
+         			/opt/lampp/lampp reloadapache
+	 			echo "\n ${green}###########################################${clean}"
+	 			echo "Successfully hosted $DOMAIN ..... "
+         			echo "${green}###########################################${clean}"
+    			fi
+   
+			sleep 1
+
+			echo "Checking whether $DOMAIN is pointed to this server......"
+     			sleep 1
+     			DNS=$(host -t A "$DOMAIN" | awk {'print $4'})
+     			SERVER=$(curl ipinfo.io/ip)
+
+			if [ "$SERVER" = "$DNS" ] ;
+    				then
+   	 				echo "\n###################################################################################"
+	 				echo  "$DOMAIN is pointed to this server, Beginning SSL Installation using certbot......"
+    	 				echo "###################################################################################"
+    	 
+    	 				echo "\n###################################################################################"
+    	 				echo  "Checking certbot is available.."
+    	 				echo "###################################################################################"
+    	 		sleep 1
+					/usr/bin/which certbot
+					if [ $? = 0 ]; then
+	    						begin_ssl
+					else
+					/usr/bin/which snapd
+            					if [ $? = 0 ]; then
+	     						snap install certbot -y
+	     						begin_ssl
+            					else
+             						apt install snapd -y
+             						snap install certbot -y
+	     						begin_ssl
+            					fi
+					fi	 
+	
+			else
+
+    			echo "${green}Please make sure to point correct records of $DOMAIN to this server  and run hostssl.sh for SSL installations......${clean}"
+    			sleep 1
+
+                        fi
+
+   		if grep -q "$DOMAIN/cert.pem" $LOC/etc/httpd.conf ; then
+			echo "\n#####################################################################################################################"
+  			echo "ssl entries of $DOMAIN already found on configuration Please manually check /opt/lampp/etc/httpd.conf !!!!!!!!!!!!!!"
+			echo "#####################################################################################################################"
+			sleep 1
+    			else
+
+				DATE=$(date +"%Y-%m-%d-%H-%M-%S")
+				cp $LOC/etc/httpd.conf $LOC/etc/httpd_backups/httpd_ssl.conf-"$DATE"
+
+			echo "\n#################################################################################################################"
+			echo " SSL entries for $DOMAIN added to configuration file. Previous configuration is copied on "$LOC/etc/httpd_backups/httpd_ssl.conf-"$DATE"" "
+			echo "#####################################################################################################################"
+
+			cat default_httpdssl.txt  >> $LOC/etc/httpd.conf
+
+			echo "Checking configuration file...."
+			echo ""
+
+			/opt/lampp/bin/apachectl -t > /dev/null 2>&1
+ 			if [ $? != 0 ]
+	     			then
+     				echo ""
+     				echo " Errors found on configuration file. Process reversed "
+     				echo ""
+     				mv $LOC/etc/httpd_backups/httpd_ssl.conf-"$DATE" /$LOC/etc/httpd.conf
+     				echo "\n ${red}SSL INSTALLATION TERMINATED Please check manually and run hostssl.sh${clean}"
+     				echo ""
+     				echo "Checking for any current syntax errors......"
+     				/opt/lampp/bin/apachectl -t
+           				if [ $? != 0 ];
+           					then echo "
+							${red}URGENT ERROR PLEASE CHECK CONFIGURATION BEFORE RELOADING SERVICE"
+           				sleep 1
+           				fi
+     			else
+	 		echo "Reloading service..."
+         		/opt/lampp/lampp reloadapache
+	 		echo ""
+	 		echo " Successfully installed SSL for  $DOMAIN ..... "
+	 		echo "Adding HTTPS redirection on .htaccess"
+	 		touch .htaccess
+	 		cat default_htaccess.txt >> .htaccess
+	 		mv .htaccess /opt/lampp/htdocs/"$DOMAIN"/
+	 		chmod 644 /opt/lampp/htdocs/$DOMAIN/.htaccess
+	 		chown -R $USER.$USER /opt/lampp/htdocs/$DOMAIN/.htaccess
+ 			fi
+		fi
 
 
-if [ -z "$DOMAIN" ]
-              then
-              printf "\nDomain name could not be empty\n"
-              echo "Try again!!!!!"
-else
+		fi
+		
 
-
-if grep -rq "ServerAlias $DOMAIN" $LOC/etc/vhosts/ ; then
-  echo "#####################################################################################################################"
-  echo "Domain already found on configuration Please manually check /opt/lampp/etc/vhosts !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"
-  echo "#####################################################################################################################"
-else
-
-echo "###############################################"
-echo "Domain name = $DOMAIN"
-echo "Document root location = $HLOC \n"
-echo "###############################################"
-
-DATE=$(date +"%Y-%m-%d-%H-%M-%S")
-
-
-
-
-printf "\nCreating directory and assigning ownership"
-mkdir -p $HLOC
-chown -R tanzanitebus.tanzanitebus $HLOC
-
-
-printf "\nDomain added to configuration. " 
-
-cat default_httpd.txt  > $LOC/etc/vhosts/$DOMAIN.conf
-
-printf "\nChecking configuration file....\n"
-
-/opt/lampp/bin/apachectl -t
- if [ $? != 0 ]
-     then
-     printf " Errors found on configuration file. Process reversed "
-     rm -rf $LOC/etc/vhosts/$DOMAIN.conf
-     rm -rf $HLOC 
-     printf "\nChecking for any current syntax errors....."
-     /opt/lampp/bin/apachectl -t
-     if [ $? != 0 ]
-           then echo " URGENT ERROR PLEASE CHECK CONFIGURATION BEFORE RELOADING SERVICE "
-           fi
-     else 
-	 echo "Reloading service..."
-         /opt/lampp/lampp reloadapache
-	 echo "###########################################"
-	 echo "Successfully hosted $DOMAIN ..... "
-         echo "###########################################"
- fi
-
-
-printf "\nChecking whether $DOMAIN is pointed to this server...... \n"
-
-SERVER=$(host -t A $DOMAIN | awk {'print $4'})
-if [ $SERVER = '41.59.227.152' ] ;
-    then
-    echo "###################################################################################"
-    echo  "$DOMAIN is pointed to this server, Beginning SSL Installation using certbot......"
-    echo "###################################################################################"
-    
-    grep $DOMAIN /$LOC/etc/vhosts/$DOMAIN.conf | grep www
-      if [ $? != 0 ]
-      then
-	     echo "SSL without www" 
-     certbot certonly --debug --webroot -w /opt/lampp/htdocs/$DOMAIN -d $DOMAIN --register-unsafely-without-email --agree-tos
-      else
-	      echo "SSL with www"
-     certbot certonly --debug --webroot -w /opt/lampp/htdocs/$DOMAIN -d $DOMAIN -d www.$DOMAIN --register-unsafely-without-email --agree-tos
-     fi     
-
-
-
-if grep -rq "$DOMAIN/cert.pem" $LOC/etc/vhosts/ ; then
-  echo "#####################################################################################################################"
-  echo "ssl entries of $DOMAIN already found on configuration Please manually check /opt/lampp/etc/httpd.conf !!!!!!!!!!!!!!"
-  echo "#####################################################################################################################"
-else
-
-DATE=$(date +"%Y-%m-%d-%H-%M-%S")
-
-cp $LOC/etc/vhosts/$DOMAIN.conf $LOC/etc/vhosts/$DOMAIN.conf-$DATE
-
-echo "#####################################################################################################################"
-echo " SSL entries for $DOMAIN added to configuration file. Previous configuration is copied on "$LOC/etc/vhosts/$DOMAIN.conf-$DATE" "
-echo "#####################################################################################################################"
-
-cat default_httpdssl.txt  >> $LOC/etc/vhosts/$DOMAIN.conf
-
-printf "\nChecking configuration file...."
-echo ""
-
-/opt/lampp/bin/apachectl -t > /dev/null 2>&1
-
- if [ $? != 0 ]
-     then
-     echo ""
-     echo " Errors found on configuration file. Process reversed "
-     echo ""
-     mv $LOC/etc/vhosts/$DOMAIN.conf-$DATE /$LOC/etc/vhosts/$DOMAIN.conf
-     printf "\n SSL INSTALLATION TERMINATED Please check manually and run hostssl.sh\n"
-     echo ""
-     printf "\nChecking for any current syntax errors......\n"
-     /opt/lampp/bin/apachectl -t
-           if [ $? != 0 ]
-           then printf "\nURGENT ERROR PLEASE CHECK CONFIGURATION BEFORE RELOADING SERVICE\n"
-           fi
-     else
-	 echo "Reloading service..."
-         /opt/lampp/lampp reloadapache
-	 echo ""
-	 echo " Successfully installed SSL for  $DOMAIN ..... "
-	 echo ""
-	 printf "\n Adding HTTPS redirection and exclude POST request on .htaccess\n"
-	 touch .htaccess
-	 cat default_htaccess.txt >> .htaccess
-	 mv .htaccess /opt/lampp/htdocs/$DOMAIN/
-	 chmod 644 /opt/lampp/htdocs/$DOMAIN/.htaccess
-	 chown tanzanitebus.tanzanitebus /opt/lampp/htdocs/$DOMAIN/.htaccess
- fi
-
-rm -rf default_httpdssl.txt default_htaccess.txt default_httpd.txt 
-
-fi
-
-else
-    printf "Please make sure to point correct records of $DOMAIN to this server (41.59.227.152) and run hostssl.sh for SSL installations......\n"
-    echo "########################################################################################"
-
-
-fi
-fi
-fi
-
-echo "###############################################"
+rm -rf default_httpdssl.txt default_htaccess.txt default_httpd.txt .htaccess default_index.txt index.html > /dev/null 2>&1
+echo "
+${green}###############################################${clean}"
 echo "Domain name = $DOMAIN"
 echo "Document root location = $HLOC "
-echo "###############################################"
-rm -rf default_httpdssl.txt default_htaccess.txt default_httpd.txt > /dev/null 2>&1
+echo "${green}###############################################${clean}
+"
+
--- hostssl.sh
+++ hostssl.sh
@@ -3,113 +3,139 @@
 #######################################
 #SCRIPT NAME		:hostssl.sh
 #DESCRIPTION		:Install ssl using letencrypt certbot and add details to configuration file
-#USAGE			    :run sh hostssl.sh
-#			        :give domain name and document root on prompt
+#USAGE			:run sh hostssl.sh
+#			:give domain name and document root on prompt
 #######################################
 
 echo "###################### SSL INSTALLATION USING CERTBOT #####################################"
 
+
 read -p " Domain : " DOMAIN
 LOC="/opt/lampp"
 HLOC="$LOC/htdocs/$DOMAIN"
+IPADDR=$(hostname -I | awk '{print $1}')
 
-cd /usr/local/scripts/hosting_script
+          cp default_httpdssl default_httpdssl.txt
+          cp default_htaccess default_htaccess.txt
+	  sed -i "s/DOMAIN/$DOMAIN/g" default_htaccess.txt
+          sed -i "s/DOMAIN/$DOMAIN/g" default_httpdssl.txt
+	  sed -i "s/IPADDR/$IPADDR/g" default_httpdssl.txt
 
-cp default_httpdssl default_httpdssl.txt
-cp default_htaccess default_htaccess.txt
+
+begin_ssl()
+
+{
+      grep "$DOMAIN" /$LOC/etc/httpd.conf | grep www
+      if [ $? != 0 ];
+      then
+      echo "SSL without www"
+      certbot certonly --debug --webroot -w /opt/lampp/htdocs/$DOMAIN -d $DOMAIN --register-unsafely-without-email --agree-tos
+      else
+      echo "SSL with www"
+      certbot certonly --debug --webroot -w /opt/lampp/htdocs/$DOMAIN -d $DOMAIN --register-unsafely-without-email --agree-tos
+      fi
+}
 
 
-
-grep -rq "ServerName $DOMAIN" $LOC/etc/vhosts/
+cat $LOC/etc/httpd.conf | grep -q "$DOMAIN"
 
 if [ $? != 0 ]
      then
      printf "\nNon-SSL entries not found for $DOMAIN.. Please run host.sh to continue....\n"
      echo "Process Terminated "
       
-elif grep -rq "$DOMAIN/cert.pem" $LOC/etc/vhosts/ ; then
+elif grep -q "$DOMAIN/cert.pem" $LOC/etc/httpd.conf ; then
      echo #####################################################################################################################
      echo ssl entries of $DOMAIN already found on configuration Please manually check /opt/lampp/etc/httpd.conf !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
      echo #####################################################################################################################
 else
 
-printf "\nChecking whether $DOMAIN is pointed to this server...... \n"
+echo "Checking whether $DOMAIN is pointed to this server......"
+     			sleep 1
+     			DNS=$(host -t A "$DOMAIN" | awk {'print $4'})
+     			SERVER=$(curl ipinfo.io/ip)
 
-SERVER=$(host -t A $DOMAIN | awk {'print $4'})
-if [ $SERVER = '41.59.227.152' ] ;
-    then
-    printf "$DOMAIN is pointed to this server, Beginning SSL Installation using certbot......\n"
-    echo " "
+			if [ "$SERVER" = "$DNS" ] ;
+    				then
+   	 				echo "\n###################################################################################"
+	 				echo  "$DOMAIN is pointed to this server, Beginning SSL Installation using certbot......"
+    	 				echo "###################################################################################"
+    	 
+    	 				echo "\n###################################################################################"
+    	 				echo  "Checking certbot is available.."
+    	 				echo "###################################################################################"
+    	 		sleep 1
+					/usr/bin/which certbot
+					if [ $? = 0 ]; then
+	    						begin_ssl
+					else
+					/usr/bin/which snapd
+            					if [ $? = 0 ]; then
+	     						snap install certbot -y
+	     						begin_ssl
+            					else
+             						apt install snapd -y
+             						snap install certbot -y
+	     						begin_ssl
+            					fi
+					fi	 
+	
+			else
 
-    grep $DOMAIN /$LOC/etc/httpd.conf | grep www
-      if [ $? != 0 ]
-      then
-	     echo " SSL without www"
-             sed -i "s/DOMAIN/$DOMAIN/g" default_htaccess.txt
-             sed -i "s/www.//g" default_htaccess.txt
-             sed -i "s/www.//g" default_httpdssl.txt
-             sed -i "s/DOMAIN/$DOMAIN/g" default_httpdssl.txt
+    			echo "${green}Please make sure to point correct records of $DOMAIN to this server  and run hostssl.sh for SSL installations......${clean}"
+    			sleep 1
 
-      certbot certonly --debug --webroot -w /opt/lampp/htdocs/$DOMAIN -d $DOMAIN --register-unsafely-without-email --agree-tos
-      else
-	      echo " SSL with www"
-	      sed -i "s/DOMAIN/$DOMAIN/g" default_htaccess.txt
-          sed -i "s/DOMAIN/$DOMAIN/g" default_httpdssl.txt
+                        fi
 
-      certbot certonly --debug --webroot -w /opt/lampp/htdocs/$DOMAIN -d $DOMAIN -d www.$DOMAIN --register-unsafely-without-email --agree-tos
-     fi
+   		if grep -q "$DOMAIN/cert.pem" $LOC/etc/httpd.conf ; then
+			echo "\n#####################################################################################################################"
+  			echo "ssl entries of $DOMAIN already found on configuration Please manually check /opt/lampp/etc/httpd.conf !!!!!!!!!!!!!!"
+			echo "#####################################################################################################################"
+			sleep 1
+    			else
 
+				DATE=$(date +"%Y-%m-%d-%H-%M-%S")
+				cp $LOC/etc/httpd.conf $LOC/etc/httpd_backups/httpd_ssl.conf-"$DATE"
 
+			echo "\n#################################################################################################################"
+			echo " SSL entres for $DOMAIN added to configuration file. Previous configuration is copied on "$LOC/etc/httpd_backups/httpd_ssl.conf-"$DATE"" "
+			echo "#####################################################################################################################"
 
-DATE=$(date +"%Y-%m-%d-%H-%M-%S")
+			cat default_httpdssl.txt  >> $LOC/etc/httpd.conf
 
-cp $LOC/etc/vhosts/$DOMAIN.conf $LOC/etc/vhosts/$DOMAIN.conf-$DATE
+			echo "Checking configuration file...."
+			echo ""
 
-sed -i "s/DOMAIN/$DOMAIN/g" default_httpdssl.txt
+			/opt/lampp/bin/apachectl -t > /dev/null 2>&1
+ 			if [ $? != 0 ]
+	     			then
+     				echo ""
+     				echo " Errors found on configuration file. Process reversed "
+     				echo ""
+     				mv $LOC/etc/httpd_backups/httpd_ssl.conf-"$DATE" /$LOC/etc/httpd.conf
+     				echo "\n ${red}SSL INSTALLATION TERMINATED Please check manually and run hostssl.sh${clean}"
+     				echo ""
+     				echo "Checking for any current syntax errors......"
+     				/opt/lampp/bin/apachectl -t
+           				if [ $? != 0 ];
+           					then echo "
+							${red}URGENT ERROR PLEASE CHECK CONFIGURATION BEFORE RELOADING SERVICE"
+           				sleep 1
+           				fi
+     			else
+	 		echo "Reloading service..."
+         		/opt/lampp/lampp reloadapache
+	 		echo ""
+	 		echo " Successfully installed SSL for  $DOMAIN ..... "
+	 		echo "Adding HTTPS redirection on .htaccess"
+	 		touch .htaccess
+	 		cat default_htaccess.txt >> .htaccess
+	 		mv .htaccess /opt/lampp/htdocs/"$DOMAIN"/
+	 		chmod 644 /opt/lampp/htdocs/$DOMAIN/.htaccess
+	 		chown -R $USER.$USER /opt/lampp/htdocs/$DOMAIN/.htaccess
+ 			fi
 
-echo ""
-echo " SSL entries for $DOMAIN added to configuration file. Current configuration is copied as "$LOC/etc/httpd_backups/httpd_ssl.conf-$DATE" "
-echo ""
-
-cat default_httpdssl.txt  >> $LOC/etc/vhosts/$DOMAIN.conf
-
-rm -f default_httpdssl.txt
-echo " checking configuration file...."
-echo ""
-
-/opt/lampp/bin/apachectl -t
-
- if [ $? != 0 ]
-     then
-     echo ""
-     echo " Errors found on configuration file. Process reversed "
-     echo ""
-     mv $LOC/etc/vhosts/$DOMAIN.conf-$DATE /$LOC/etc/vhosts/$DOMAIN.conf
-     echo " SSL INSTALLATION TERMINATED!!!!!!!!!!!!!"
-     echo ""
-     echo "Checking for any current syntax errors......"
-     /opt/lampp/bin/apachectl -t
-           if [ $? != 0 ]
-           then echo " URGENT ERROR PLEASE CHECK CONFIGURATION BEFORE RELOADING SERVICE "
-           fi     
-     else
-	 echo "Reloading service..."
-         /opt/lampp/lampp reloadapache
-	 echo ""
-	 echo " Successfully installed SSL for  $DOMAIN ..... "
-	 echo ""
-	 printf "\n Adding HTTPS redirection and exclude POST request on .htaccess\n"
-         touch .htaccess
-         cat default_htaccess.txt >> .htaccess
-         mv .htaccess /opt/lampp/htdocs/$DOMAIN/
-         chmod 644 /opt/lampp/htdocs/$DOMAIN/.htaccess
-         chown tanzanitebus.tanzanitebus /opt/lampp/htdocs/$DOMAIN/.htaccess
- fi
-else echo "Please check $DOMAIN have correct records pointed to this domain and try again"
-rm -f default_httpdssl.txt default_htaccess.txt > /dev/null 2>&1
+fi		
 fi
-fi
 
-echo "#############################################################"
-
-rm -f default_httpdssl.txt default_htaccess.txt > /dev/null 2>&1
+rm -rf default_htaccess.txt default_httpdssl.txt
